4c606d5e07f32cafe384b96409d2046b9da68e72
Guy Halperin <geh4y8@gmail.com>
2017-10-05 20:46:26 -0700

    initial commit

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,18 @@
+class WebhooksController < ApplicationController
+  # skip_before_filter :verify_authenticity_token
+
+  def receive
+    if request.headers['Content-Type'] == 'application/json'
+      data = JSON.parse(request.body.read)
+      puts data
+    else
+      # application/x-www-form-urlencoded
+      data = params.as_json
+      puts data
+    end
+
+    # Webhook::Received.save(data: data, integration: params[:integration_name])
+
+    render nothing: true
+  end
+end
---
+++ config/slack.rb
@@ -0,0 +1,3 @@
+Slack.configure do |config|
+  config.token = 'xoxp-252378819380-252378819684-252464712677-3a8c9a4d80f38aae088426916e2a1f89'
+end
